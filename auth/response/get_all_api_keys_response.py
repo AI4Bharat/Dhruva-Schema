93374538faa3077d94e20cd376b911c08e86b544
Hash <47293898+ashwin-014@users.noreply.github.com>
2023-03-08 11:44:54 +0530

    Schema changes to support Metering APIs

--- auth/response/get_all_api_keys_response.py
+++ auth/response/get_all_api_keys_response.py
@@ -1,6 +1,8 @@
 from typing import List, Optional
-
+from bson import ObjectId
 from pydantic import BaseModel
+from db.MongoBaseModel import ObjectIdField
+from schema.auth.common import RoleType
 
 
 class _ServiceUsage(BaseModel):
@@ -9,20 +11,24 @@ class _ServiceUsage(BaseModel):
 
 class _ApiKey(BaseModel):
     # TODO: Add this field back once _id is correctly loaded in ApiKey model
-    # id: str
-    name: Optional[str]
-    masked_key: Optional[str]
-    active: Optional[bool]
-    type: Optional[str]
-    # created_on: str
-    # created_by: str
-    usage: Optional[int]
+    id: ObjectIdField  # str
+    name: str
+    masked_key: str
+    active: bool
+    type: str
+    created_on: str
+    usage: int = 0
     services: Optional[List[_ServiceUsage]]
+    role_type: Optional[RoleType]
+
+    class Config:
+        arbitrary_types_allowed = True
+        json_encoders = {ObjectIdField: str, RoleType: str}
 
 
 class GetAllApiKeysResponse(BaseModel):
     api_keys: List[_ApiKey]
-    total_usage: int
+    total_usage: int = 0
     page: Optional[int]
     limit: Optional[int]
     total_pages: int
--- services/response/service_view_response.py
+++ services/response/service_view_response.py
@@ -1,6 +1,9 @@
+from typing import List
 from module.services.model import Model
+from ...auth.response.get_all_api_keys_response import _ApiKey
 from .service_response import ServiceResponse
 
 class ServiceViewResponse(ServiceResponse):
     model: Model
-    usage: int = 0
+    key_usage: List[_ApiKey] = []
+    total_usage: int = 0
